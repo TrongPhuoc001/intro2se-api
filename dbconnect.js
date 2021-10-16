726ecf60b435b235d981d3e929c6398a4f4b8ea0
TrongPhuoc001 <85543424+TrongPhuoc001@users.noreply.github.com>
2021-10-16 23:38:51 +0700

    byvrypt change

--- dbconnect.js
+++ dbconnect.js
@@ -1,21 +1,21 @@
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// client.connect();
-const Pool = require("pg").Pool;
-
-const pool = new Pool({
-    user: "postgres",
-    password: "phuoc190301",
-    database: "eclass",
-    host: "localhost",
-    port: 5432
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+client.connect();
+// const Pool = require("pg").Pool;
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "phuoc190301",
+//     database: "eclass",
+//     host: "localhost",
+//     port: 5432
+// });
 
 module.exports = pool;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const pool = require("../dbconnect");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { registerValid, loginValid } = require("../validation");
 const verify = require("../verifyToken");

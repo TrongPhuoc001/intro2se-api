b2168f1a4a5cbd643665030370f70e527662c838
TrongPhuoc001 <85543424+TrongPhuoc001@users.noreply.github.com>
2021-10-18 22:05:12 +0700

    admin add

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,24 +1,41 @@
 const data = JSON.parse(table_data.replaceAll('&#34;', '"'));
+
+
 for(const key in data){
     const item_div = document.createElement('div');
-    item_div.textContent = data[key];
+    item_div.textContent = key;
     item_div.className = "list-item";
-    item_div.onclick = ()=>{
+    item_div.value = key;
+    item_div.onclick = ()=>{ 
+        document.querySelector('#add-btn').setAttribute('data-tb_name',key);
         document.querySelectorAll('.list-item').forEach(div=>{
-            div.style.backgroundColor = "#ffffff";
+            div.style.backgroundColor = "inherit";
         })
         item_div.style.backgroundColor ="#6e6e6e";
         document.querySelector('.table-content').innerHTML ="";
-        document.querySelector('.content h2').textContent = data[key];
-        fetch(`./dashboard/${data[key]}`)
+        document.querySelector('.form').innerHTML = '';
+        document.querySelector('.content h2').textContent = key;
+        document.querySelector('.loader').style.display = 'block';      
+        const form = makeInput(data[key])
+        document.querySelector('.form').innerHTML+=form;
+        fetch(`./dashboard/${key}`)
         .then(res => res.json())
         .then(data_list => {
+            document.querySelector('.loader').style.display = 'none';
             if(data_list.length > 0){
-                const tr = objToth(data_list[0]);
+                const tr = tableHeader(data_list[0]);
                 document.querySelector('.table-content').innerHTML+=tr;
                 for(let i=0;i<data_list.length;i++){
+                    let tr_class = '';
+                    if(i%2===0){
+                        tr_class = ' class="even-row"';
+                    }
+                    else{
+                        tr_class = ' class="odd-row"';
+                    }
                     const row = data_list[i];
-                    const tr = objTotr(row);
+                    let tr = objTotr(row);
+                    tr = tr.substring(0,3) + tr_class +tr.substring(3);
                     document.querySelector('.table-content').innerHTML+=tr;
                 }
             }
@@ -30,12 +47,48 @@ for(const key in data){
     }
     document.querySelector('.table-list').append(item_div);
 }
-function objToth(obj){
+
+document.querySelector('#add-btn').addEventListener('click', ()=>{
+    const body_inp = {};
+    document.querySelectorAll('.add-form input').forEach(input=>{
+        if(input.value === '') return;
+        body_inp[`${input.name}`] = input.value;
+        input.value = '';
+    });
+    const tb_name = document.querySelector('#add-btn').dataset.tb_name;
+    const tr = objTotr(body_inp);  
+    fetch(`./dashboard/${tb_name}`, {
+        'method':'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body:JSON.stringify(body_inp)
+    })
+    .then(res=>res.json())
+    .then(mess =>{
+        if(mess){
+            console.log(mess);
+            document.querySelectorAll('.list-item').forEach(div=>{if(div.value===tb_name){div.click()}});
+        }
+    })
+});
+
+function makeInput(arr){
+    let form = '<form class="add-form" autocomplete="off">';
+    for(key in arr){
+        form+=`<input type="text" name="${arr[key]}" placeholder="${arr[key]}">`; 
+    }
+    form+='</form>'
+    return form
+}
+function tableHeader(obj){
+    
     let tr = '<tr>';
     for(key in obj){
         tr+='<th>'+ key + '</th>';
     }
     tr+='</tr>';
+    
     return tr;
 }
 function objTotr (obj){
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const pool = require("../dbconnect");
+const {registerValid, courseValid} = require('../validation');
 const router = express.Router();
 let auth = false;
 
@@ -22,7 +23,12 @@ router.get('/login', (req, res) => {
     res.render('login', { message: req.query.message});
 })
 router.get('/dashboard',check_auth,(req,res)=>{
-    const arr =['courses', 'courses_stu','subject','users'];
+    const arr ={
+        courses :['name', 'creator_id', 'subject_id', 'time_start', 'time_end', 'day_study', 'max_slot', 'fee'],
+        courses_stu: ['stu_id', 'course_id'],
+        subject:['name'],
+        users:['name','email','password','type','gender','birth']
+    };
     res.render('index',{data:JSON.stringify(arr)});
 })
 router.get('/dashboard/:table_name', (req,res)=>{
@@ -52,70 +58,78 @@ router.delete('/dashboard/:table_name/:record_id', (req,res)=>{
 
 router.post('/dashboard/:table_name', async (req,res)=>{
     const tb_name = req.params.table_name;
-    switch(tb_name){
-        case 'courses':
-            const {name, subject_id, time_start, time_end, day_study, max_slot,fee} = req.body;
-            pool.query(
-                `INSERT INTO courses(name, creator_id, subject_id, time_start, time_end, day_study, max_slot,fee)
-                VALUES ($1,$2,$3,$4,$5,$6,$7,$8);`, [name,req.user._id, subject_id, time_start, time_end, day_study, max_slot,fee], (err,result)=>{
-                    if(err){
-                        return res.json(err.routine);
-                    }
-                    return res.status(200).json({"message":"create success"});
+    if(tb_name==='courses'){
+        const {error} = courseValid(req.body);
+        if(error){
+            return res.status(400).json({"message" :error.details[0].message});
+        }
+        const {name, subject_id, time_start, time_end, day_study, max_slot,fee} = req.body;
+        pool.query(
+            `INSERT INTO courses(name, creator_id, subject_id, time_start, time_end, day_study, max_slot,fee)
+            VALUES ($1,$2,$3,$4,$5,$6,$7,$8);`, [name,req.user._id, subject_id, time_start, time_end, day_study, max_slot,fee], (err,result)=>{
+                if(err){
+                    return res.json(err.routine);
                 }
-            )
-            break;
-        case 'courses_stu':
-            const {stu_id,course_id} = req.body;
-            pool.query(
-                `INSERT INTO courses_stu(stu_id,course_id)
-                VALUES ($1,$2);`,[stu_id,course_id],(err,results)=>{
-                    if(err){
-                        return res.status(400).json(err.routine);
-                    }
-                    return res.status(200).json({"message":"create success"});
-                }
-            )
-            break;
-        case 'subject':
-            const {sub_name} = req.body;
-            pool.query(
-                `INSERT INTO subject(name)
-                VALUES ($1);`,[sub_name],(err,results)=>{
-                    if(err){
-                        return res.status(400).json(err.routine);
-                    }
-                    return res.status(200).json({"message":"create success"});
-                }
-            )
-            break;
-        case 'users':
-            const {user_name , email, password, type, gender, birth} = req.body;
-            const hashedPassword = await bcrypt.hash(password, 10);
-            pool.query(
-                `SELECT * FROM users
-                WHERE email = $1;`, [email], (err,result)=>{
-                    if(err){
-                        return res.status(400).json(err);
-                    }
-                    if(result.rows.length > 0 ){
-                        return res.status(400).json({"message": "email has already been taken"});
-                    }
-                    else {
-                        pool.query(
-                            `INSERT INTO users(name,email,password,type, gender, birth)
-                            VALUES ($1,$2,$3,$4,$5,$6);`, [user_name,email,hashedPassword,parseInt(type),gender,birth], (err,result)=>{
-                                if(err){
-                                    return res.json(err);
-                                }
-                                return res.status(200).json({"message": "Account create successful"});
-                            }
-                        )
-                    }
-                }
-            )
-            break;
+                return res.status(200).json({"message":"create success"});
+            }
+        );
     }
+    else if(tb_name === 'courses_stu'){
+        const {stu_id,course_id} = req.body;
+        pool.query(
+            `INSERT INTO courses_stu(stu_id,course_id)
+            VALUES ($1,$2);`,[stu_id,course_id],(err,results)=>{
+                if(err){
+                    return res.status(400).json(err.routine);
+                }
+                return res.status(200).json({"message":"create success"});
+            }
+        )
+    }
+    else if(tb_name === 'subject'){
+        const {name} = req.body;
+        console.log(name);
+        pool.query(
+            `INSERT INTO subject(name)
+            VALUES ($1);`,[name],(err,results)=>{
+                if(err){
+                    return res.status(400).json(err.routine);
+                }
+                return res.status(200).json({"message":"create success"});
+            }
+        )
+    }
+    else if(tb_name==='users'){
+        const {user_error} = registerValid(req.body);
+        if(error){
+            return res.status(400).json({"message" :user_error.details[0].message})
+        }
+        const {name , email, password, type, gender, birth} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        pool.query(
+            `SELECT * FROM users
+            WHERE email = $1;`, [email], (err,result)=>{
+                if(err){
+                    return res.status(400).json(err);
+                }
+                if(result.rows.length > 0 ){
+                    return res.status(400).json({"message": "email has already been taken"});
+                }
+                else {
+                    pool.query(
+                        `INSERT INTO users(name,email,password,type, gender, birth)
+                        VALUES ($1,$2,$3,$4,$5,$6);`, [user_name,email,hashedPassword,parseInt(type),gender,birth], (err,result)=>{
+                            if(err){
+                                return res.json(err);
+                            }
+                            return res.status(200).json({"message": "Account create successful"});
+                        }
+                    )
+                }
+            }
+        )
+    }
+    
 })
 
 router.post('/login', (req,res)=>{
